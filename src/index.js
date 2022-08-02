0e43ce1ac60b0a733c0e68485c9537421684cd74
dikhorsun <dmitry.horsun@gmail.com>
2022-08-02 22:57:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,11 +35,30 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '*****':  ' ',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let resultArr = [];
+    for(let i=0; i<expr.length; i+=10){
+        let letterTen = expr.substr(i, 10);
+        let resultMini = '';
+  
+        for(let j=0; j<letterTen.length; j+=2){
+            let letterTwo = letterTen.substr(j, 2);
+            if(letterTwo === "10"){
+              resultMini += '.'
+            } else if(letterTwo === "11"){
+              resultMini += '-'
+            } else if(letterTwo === "**"){
+              resultMini += '*'
+            }
+        }
+        resultArr.push(resultMini)
+    }
+    return resultArr.map(v => MORSE_TABLE[v]).join('')
+  }
+
 
 module.exports = {
     decode
